cda752ed6ee95958856718254af754190211c8f7
Tan Chia Chun <54241577+TanChiaChun@users.noreply.github.com>
2022-03-11 00:24:17 +0800

    Add Money to isAnyFieldEdited()

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -153,7 +153,7 @@ public class EditCommand extends Command {
          * Returns true if at least one field is edited.
          */
         public boolean isAnyFieldEdited() {
-            return CollectionUtil.isAnyNonNull(name, phone, email, address, tags);
+            return CollectionUtil.isAnyNonNull(name, phone, email, address, money, tags);
         }
 
         public void setName(Name name) {
