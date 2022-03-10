efc2b48178fca897085094f827a4c1501ed1ec11
Tan Chia Chun <54241577+TanChiaChun@users.noreply.github.com>
2022-03-10 22:25:46 +0800

    Add constructor

--- src/main/java/seedu/address/model/person/Money.java
+++ src/main/java/seedu/address/model/person/Money.java
@@ -1,8 +1,20 @@
 package seedu.address.model.person;
 
+import static java.util.Objects.requireNonNull;
+
 /**
  * Represents money owed to a Person in the address book.
  */
 public class Money {
     private float value;
+
+    /**
+     * Constructs a {@code Money}.
+     *
+     * @param value A value.
+     */
+    public Money(float value) {
+        requireNonNull(value);
+        this.value = value;
+    }
 }
