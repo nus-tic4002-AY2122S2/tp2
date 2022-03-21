ee922fa0e85d198c7d2167b9667c37eca52cd829
l-shihao <e0166067@u.nus.edu>
2022-03-21 15:36:55 +0800

    Pause to do docs

---
+++ src/main/java/seedu/address/model/person/Relation.java
@@ -0,0 +1,12 @@
+package seedu.address.model.person;
+
+import java.util.logging.Logger;
+
+import seedu.address.commons.core.LogsCenter;
+
+
+public class Graph {
+    private static final Logger logger = LogsCenter.getLogger(Graph.class);
+
+
+}
