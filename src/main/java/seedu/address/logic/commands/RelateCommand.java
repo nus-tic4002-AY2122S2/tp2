8575ceafb30c34561f2531e48aa4f381cbdd0b8d
l-shihao <e0166067@u.nus.edu>
2022-03-21 15:38:20 +0800

    Pause to do docs

--- src/main/java/seedu/address/logic/commands/RelateCommand.java
+++ src/main/java/seedu/address/logic/commands/RelateCommand.java
@@ -1,2 +1,32 @@
-package seedu.address.logic.commands;public class RelateCommand {
+package seedu.address.logic.commands;
+
+import static seedu.address.logic.parser.CliSyntax.PREFIX_RELATE;
+
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.Model;
+import seedu.address.model.ReadOnlyAddressBook;
+
+
+public class RelateCommand extends Command {
+
+    public static final String COMMAND_WORD = "relate";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": 1 to n mutual relationship "
+            + "by the index number used in the displayed person list.\n"
+            + "Parameters: INDEX (must be a positive integer) "
+            + PREFIX_RELATE + " "
+            + "[INDEX1] "
+            + "[INDEX2] "
+            + "[INDEX3]\n"
+            + "Example: " + COMMAND_WORD + " 1 "
+            + PREFIX_RELATE + "2 3 4 ";
+
+    @Override
+    public CommandResult execute(Model model, ReadOnlyAddressBook
+            originalAddressBook, String exCommand) throws CommandException,
+            ParseException {
+        return null;
+    }
 }
--- src/main/java/seedu/address/model/person/Relation.java
+++ src/main/java/seedu/address/model/person/Relation.java
@@ -5,8 +5,10 @@ import java.util.logging.Logger;
 import seedu.address.commons.core.LogsCenter;
 
 
-public class Graph {
-    private static final Logger logger = LogsCenter.getLogger(Graph.class);
+public class Relation {
+    public static final String MESSAGE_CONSTRAINTS = "Should provide valid index numbers";
+    private static final Logger logger = LogsCenter.getLogger(Relation.class);
+
 
 
 }
