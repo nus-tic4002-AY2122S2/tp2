d25e5b832493563bdff76a6c1b7bab4ab8b1ba09
jr-mojito <chunyg335@gmail.com>
2022-03-24 21:43:19 +0800

    Checkstyle error

--- src/main/java/seedu/address/logic/commands/SortCommand.java
+++ src/main/java/seedu/address/logic/commands/SortCommand.java
@@ -1,39 +1,36 @@
-package seedu.address.logic.commands;
+    package seedu.address.logic.commands;
 
-import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
-import static java.util.Objects.requireNonNull;
+    import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
+    import static java.util.Objects.requireNonNull;
 
-import java.util.Comparator;
-import java.util.logging.Logger;
+    import java.util.Comparator;
 
-import seedu.address.commons.core.LogsCenter;
-import seedu.address.model.Model;
-import seedu.address.model.ReadOnlyAddressBook;
-import seedu.address.model.person.Name;
-import seedu.address.model.person.Person;
+    import seedu.address.model.Model;
+    import seedu.address.model.ReadOnlyAddressBook;
+    import seedu.address.model.person.Name;
+    import seedu.address.model.person.Person;
 
-public abstract class SortCommand extends Command implements Comparable<Person> {
+    public abstract class SortCommand extends Command implements Comparable<Person> {
 
-    public static final String COMMAND_WORD = "sort";
-    public static final String MESSAGE_SUCCESS = "Sorted all persons";
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Sort all persons by names \n"
-                                                            + "Example: " + COMMAND_WORD + "";
-
-    @Override
-    public CommandResult execute(Model model, ReadOnlyAddressBook originalAddressBook, String exCommand) {
-        requireNonNull(model);
-        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
-        return new CommandResult(MESSAGE_SUCCESS);
-    }
-
-    public static Comparator<Person> ascComparator = new Comparator<Person>() {
+        public static final String COMMAND_WORD = "sort";
+        public static final String MESSAGE_SUCCESS = "Sorted all persons";
+        public static final String MESSAGE_USAGE = COMMAND_WORD + ": Sort all persons by names \n";
 
         @Override
-        public int compare(Person o1, Person o2) {
-            Name p1 = o1.getName();
-            Name p2 = o2.getName();
-
-            return p1.fullName.compareTo(p2.fullName);
+        public CommandResult execute(Model model, ReadOnlyAddressBook originalAddressBook, String exCommand) {
+            requireNonNull(model);
+            model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+            return new CommandResult(MESSAGE_SUCCESS);
         }
-    };
-}
+
+        public static Comparator<Person> ascComparator = new Comparator<Person>() {
+
+            @Override
+            public int compare(Person o1, Person o2) {
+                Name p1 = o1.getName();
+                Name p2 = o2.getName();
+
+                return p1.fullName.compareTo(p2.fullName);
+            }
+        };
+    }
