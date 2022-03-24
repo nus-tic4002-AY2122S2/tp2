5a250dd2cb0d36d526e024b1b60af1dc4e523985
yinyin377 <54065563+yinyin924@users.noreply.github.com>
2022-03-24 18:59:02 +0800

    enhance RenameCommand to rename from one tag into N tag

--- src/main/java/seedu/address/logic/commands/RenameCommand.java
+++ src/main/java/seedu/address/logic/commands/RenameCommand.java
@@ -1,45 +1,235 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
-import seedu.address.commons.core.Messages;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import seedu.address.commons.util.CollectionUtil;
+import seedu.address.logic.LogicManager;
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.ReadOnlyAddressBook;
-import seedu.address.model.person.NameContainsTagPredicate;
-
+import seedu.address.model.person.Address;
+import seedu.address.model.person.Birthday;
+import seedu.address.model.person.Email;
+import seedu.address.model.person.Name;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.Phone;
+import seedu.address.model.tag.Tag;
 
 /**
- * Finds and lists all persons in address book whose name contains any of the argument keywords.
- * Keyword matching is case insensitive.
+ * Edits the details of an existing person in the address book.
  */
 public class RenameCommand extends Command {
 
     public static final String COMMAND_WORD = "rename";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Rename all persons whose tag contain any of "
-            + "the specified tag and displays them as a list with index numbers.\n"
-            + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
-            + "Example: " + COMMAND_WORD + " class";
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": rename the tag of the person identified "
+            + "by the existing tag in the displayed person list. "
+            + "Existing values will be overwritten by the input values.\n"
+            + "Parameters: TAG (existing tag to rename) "
+            + "[" + PREFIX_TAG + "TAG]...\n"
+            + "Example: " + COMMAND_WORD + " g501 "
+            + PREFIX_TAG + "g603 ";
 
-    private final NameContainsTagPredicate predicate;
+    public static final String MESSAGE_RENAME_SUCCESS = "Successfully updated tag";
+    public static final String MESSAGE_NOT_RENAMED = "At least one field to rename must be provided.";
+    public static final String MESSAGE_DUPLICATE_PERSON = "This person already exists in the address book.";
 
-    public RenameCommand(NameContainsTagPredicate predicate) {
-        this.predicate = predicate;
+    private final EditPersonDescriptor editPersonDescriptor;
+    private final Tag keyword;
+    /**
+     * @param editPersonDescriptor details to edit the person with
+     * @param keyword tag to rename with
+     */
+    public RenameCommand(EditPersonDescriptor editPersonDescriptor, Tag keyword) {
+        requireNonNull(editPersonDescriptor);
+        requireNonNull(keyword);
+
+        this.editPersonDescriptor = new EditPersonDescriptor(editPersonDescriptor);
+        this.keyword = keyword;
     }
 
     @Override
     public CommandResult execute(Model model, ReadOnlyAddressBook originalAddressBook,
-                                    String exCommand) {
+                                 String exCommand) throws CommandException {
         requireNonNull(model);
-        model.updateFilteredPersonList(predicate);
-        return new CommandResult(
-                String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, model.getFilteredPersonList().size()));
+        List<Person> lastShownList = model.getFilteredPersonList();
+        int index;
+        for(index = 0; index < lastShownList.size(); index++) {
+            Person personToEdit = lastShownList.get(index);
+            Person editedPerson = createEditedPerson(personToEdit, editPersonDescriptor, keyword);
+
+            if (!personToEdit.isSamePerson(editedPerson) && model.hasPerson(editedPerson)) {
+                throw new CommandException(MESSAGE_DUPLICATE_PERSON);
+            }
+
+            LogicManager.updateOriginalAddressBook(model.getAddressBook());
+
+            model.setPerson(personToEdit, editedPerson);
+            model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+        }
+        return new CommandResult(String.format(MESSAGE_RENAME_SUCCESS));
+    }
+
+    /**
+     * Creates and returns a {@code Person} with the details of {@code personToEdit}
+     * edited with {@code editPersonDescriptor}.
+     */
+    private static Person createEditedPerson(Person personToEdit, EditPersonDescriptor editPersonDescriptor, Tag keyword) {
+        assert personToEdit != null;
+
+        Name updatedName = editPersonDescriptor.getName().orElse(personToEdit.getName());
+        Birthday updateBirthday = editPersonDescriptor.getBirthday().orElse(personToEdit.getBirthday());
+        Phone updatedPhone = editPersonDescriptor.getPhone().orElse(personToEdit.getPhone());
+        Email updatedEmail = editPersonDescriptor.getEmail().orElse(personToEdit.getEmail());
+        Address updatedAddress = editPersonDescriptor.getAddress().orElse(personToEdit.getAddress());
+        Set<Tag> updatedTags = editPersonDescriptor.getTags().orElse(personToEdit.getTags());
+        Set<Tag> personTags = personToEdit.getTags();
+        Set<Tag> copy = new HashSet<>();
+        copy.addAll(updatedTags);
+        copy.addAll(personTags);
+
+        for (Tag temp:copy) {
+                if(keyword.equals(temp)){
+                    copy.remove(temp);
+                    return new Person(updatedName,updatedPhone,updatedEmail,updatedAddress,copy,updateBirthday);
+            }
+        }
+        return new Person(updatedName,updatedPhone,updatedEmail,updatedAddress,personTags,updateBirthday);
     }
 
     @Override
     public boolean equals(Object other) {
-        return other == this // short circuit if same object
-                || (other instanceof RenameCommand // instanceof handles nulls
-                && predicate.equals(((RenameCommand) other).predicate)); // state check
+        // short circuit if same object
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof EditCommand)) {
+            return false;
+        }
+
+        // state check
+        RenameCommand e = (RenameCommand) other;
+        return editPersonDescriptor.equals(e.editPersonDescriptor);
+    }
+
+    /**
+     * Stores the details to edit the person with. Each non-empty field value will replace the
+     * corresponding field value of the person.
+     */
+    public static class EditPersonDescriptor {
+        private Name name;
+        private Phone phone;
+        private Email email;
+        private Address address;
+        private Set<Tag> tags;
+        private Birthday birthday;
+
+        public EditPersonDescriptor() {}
+
+
+
+        /**
+         * Copy constructor.
+         * A defensive copy of {@code tags} is used internally.
+         */
+        public EditPersonDescriptor(EditPersonDescriptor toCopy) {
+            setName(toCopy.name);
+            setPhone(toCopy.phone);
+            setEmail(toCopy.email);
+            setAddress(toCopy.address);
+            setTags(toCopy.tags);
+            setBirthday(toCopy.birthday);
+        }
+
+        /**
+         * Returns true if tag is edited.
+         */
+        public boolean isAnyFieldEdited() {
+            return CollectionUtil.isAnyNonNull(tags);
+        }
+
+        public void setName(Name name) {
+            this.name = name;
+        }
+
+        public Optional<Name> getName() {
+            return Optional.ofNullable(name);
+        }
+
+        public void setPhone(Phone phone) {
+            this.phone = phone;
+        }
+
+        public Optional<Phone> getPhone() {
+            return Optional.ofNullable(phone);
+        }
+
+        public void setEmail(Email email) {
+            this.email = email;
+        }
+
+        public Optional<Email> getEmail() {
+            return Optional.ofNullable(email);
+        }
+
+        public void setAddress(Address address) {
+            this.address = address;
+        }
+
+        public Optional<Address> getAddress() {
+            return Optional.ofNullable(address);
+        }
+
+        public void setBirthday(Birthday birthday) {
+            this.birthday = birthday;
+        }
+
+        public Optional<Birthday> getBirthday() {
+            return Optional.ofNullable(birthday);
+        }
+        /**
+         * Sets {@code tags} to this object's {@code tags}.
+         * A defensive copy of {@code tags} is used internally.
+         */
+        public void setTags(Set<Tag> tags) {
+            this.tags = (tags != null) ? new HashSet<>(tags) : null;
+        }
+
+        /**
+         * Returns an unmodifiable tag set, which throws {@code UnsupportedOperationException}
+         * if modification is attempted.
+         * Returns {@code Optional#empty()} if {@code tags} is null.
+         */
+        public Optional<Set<Tag>> getTags() {
+            return (tags != null) ? Optional.of(Collections.unmodifiableSet(tags)) : Optional.empty();
+        }
+
+        @Override
+        public boolean equals(Object other) {
+            // short circuit if same object
+            if (other == this) {
+                return true;
+            }
+
+            // instanceof handles nulls
+            if (!(other instanceof EditPersonDescriptor)) {
+                return false;
+            }
+
+            // state check
+            EditPersonDescriptor e = (EditPersonDescriptor) other;
+
+            return getTags().equals(e.getTags());
+        }
     }
 }
