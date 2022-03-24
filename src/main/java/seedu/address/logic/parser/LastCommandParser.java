17345127cf8422cf843db8132f17dcc57f840127
Joseph <ariesyang92@gmail.com>
2022-03-24 23:14:52 +0800

    last student

---
+++ src/main/java/seedu/address/logic/parser/LastCommandParser.java
@@ -0,0 +1,28 @@
+package seedu.address.logic.parser;
+
+import seedu.address.logic.commands.LastCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+/**
+ * Parses input arguments and creates a new FindCommand object
+ */
+public class LastCommandParser implements Parser<LastCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the FindCommand
+     * and returns a FindCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public LastCommand parse(String args) throws ParseException {
+        String trimmedArgs = args.trim();
+        if (trimmedArgs.isEmpty()) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, LastCommand.MESSAGE_USAGE));
+        }
+
+        return new LastCommand(trimmedArgs);
+    }
+
+}
