8308cd8661dd6596cbeb90c1590f6cec7f01c2d2
jr-mojito <chunyg335@gmail.com>
2022-03-24 21:35:23 +0800

    Checkstyle error

--- src/main/java/seedu/address/logic/commands/SortCommand.java
+++ src/main/java/seedu/address/logic/commands/SortCommand.java
@@ -2,33 +2,30 @@ package seedu.address.logic.commands;
 
 import java.util.Comparator;
 import java.util.logging.Logger;
+
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.model.Model;
 import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
 
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 import static java.util.Objects.requireNonNull;
 
-import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
-
-public abstract class SortCommand extends Command implements Comparable<Person>{
+public abstract class SortCommand extends Command implements Comparable<Person> {
 
     private static final Logger logger = LogsCenter.getLogger(SortCommand.class);
 
     public static final String COMMAND_WORD = "sort";
     public static final String MESSAGE_SUCCESS = "Sorted all persons";
-
-
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Sort all persons by names \n"
-//            + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
             + "Example: " + COMMAND_WORD + "";
 
     @Override
     public CommandResult execute(Model model, ReadOnlyAddressBook originalAddressBook, String exCommand) {
         requireNonNull(model);
         model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
-//        Collections.sort(originalAddressBook.getPersonList(), ascComparator);
+//      Collections.sort(originalAddressBook.getPersonList(), ascComparator);
         return new CommandResult(MESSAGE_SUCCESS);
     }
 
@@ -42,5 +39,4 @@ public abstract class SortCommand extends Command implements Comparable<Person>{
             return p1.fullName.compareTo(p2.fullName);
         }
     };
-
 }
