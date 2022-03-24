7a95787347662f49c3b1126c784bfd2eb7114c9e
jr-mojito <chunyg335@gmail.com>
2022-03-24 21:26:38 +0800

    Checkstyle error

--- src/main/java/seedu/address/logic/commands/SortCommand.java
+++ src/main/java/seedu/address/logic/commands/SortCommand.java
@@ -1,22 +1,11 @@
 package seedu.address.logic.commands;
 
-import java.util.Collections;
 import java.util.Comparator;
-import java.util.Date;
-import java.util.List;
 import java.util.logging.Logger;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.collections.transformation.SortedList;
-import seedu.address.MainApp;
 import seedu.address.commons.core.LogsCenter;
-import seedu.address.commons.core.Messages;
-import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ReadOnlyAddressBook;
-import seedu.address.model.person.Address;
 import seedu.address.model.person.Name;
-import seedu.address.model.person.NameContainsKeywordsPredicate;
 import seedu.address.model.person.Person;
 
 import static java.util.Objects.requireNonNull;
@@ -36,31 +25,12 @@ public abstract class SortCommand extends Command implements Comparable<Person>{
             + "Example: " + COMMAND_WORD + "";
 
     @Override
-    public CommandResult execute(Model model, ReadOnlyAddressBook originalAddressBook,
-                                 String exCommand) {
+    public CommandResult execute(Model model, ReadOnlyAddressBook originalAddressBook, String exCommand) {
         requireNonNull(model);
         model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
-
 //        Collections.sort(originalAddressBook.getPersonList(), ascComparator);
-
         return new CommandResult(MESSAGE_SUCCESS);
     }
-
-//    @Override
-//    public int compare(ReadOnlyAddressBook o1, ReadOnlyAddressBook o2) {
-//        ObservableList<Person> p1 = o1.getPersonList();
-//        ObservableList<Person> p2= o2.getPersonList();
-//
-//        Comparator<ReadOnlyAddressBook> studentComparator = Comparator.comparing(ReadOnlyAddressBook::toString);
-//        ObservableList<ReadOnlyAddressBook> allStudentsWithStatus = FXCollections.observableArrayList();
-//
-////    Collections.sort(allStudentsWithStatus, studentComparator);
-//
-//        SortedList<ReadOnlyAddressBook> sortedStudents = new SortedList<>(allStudentsWithStatus, studentComparator);
-//        logger.info("==== "+sortedStudents);
-//
-//        return 1;
-//    }
 
     public static Comparator<Person> ascComparator = new Comparator<Person>() {
 
@@ -71,24 +41,6 @@ public abstract class SortCommand extends Command implements Comparable<Person>{
 
             return p1.fullName.compareTo(p2.fullName);
         }
-
-//        Comparator<ReadOnlyAddressBook> studentComparator = Comparator.comparing(ReadOnlyAddressBook::toString);
-//        ObservableList<ReadOnlyAddressBook> allStudentsWithStatus = FXCollections.observableArrayList();
-//        SortedList<ReadOnlyAddressBook> sortedStudents = new SortedList<>(allStudentsWithStatus, studentComparator);
-//        logger.info("==== "+sortedStudents);
-//
-//        return 1;
-//    }
-
-//    public static Comparator<Person> ascComparator = new Comparator<Person>() {
-//        @Override
-//        public int compare(Person o1, Person o2) {
-//            Name person1 = o1.getName();
-//            Name person2 = o2.getName();
-//
-//            return person1.fullName.compareTo(person2.fullName);
-//        }
     };
-
 
 }
