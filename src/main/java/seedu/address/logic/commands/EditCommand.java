96afdd8c2a03a00ce836c65cf4a9c321214adc58
Joseph <ariesyang92@gmail.com>
2022-03-24 22:24:49 +0800

    checkstyle

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -1,18 +1,40 @@
 package seedu.address.logic.commands;
 
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_CLASSROOM;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ENGLISH;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_MATHEMATICS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_MOTHERTONGUE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_SCIENCE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.commons.util.CollectionUtil;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
-import seedu.address.model.person.*;
+import seedu.address.model.person.Address;
+import seedu.address.model.person.Classroom;
+import seedu.address.model.person.Email;
+import seedu.address.model.person.English;
+import seedu.address.model.person.Mathematics;
+import seedu.address.model.person.MotherTongue;
+import seedu.address.model.person.Name;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.Phone;
+import seedu.address.model.person.Science;
 import seedu.address.model.tag.Tag;
-
-import java.util.*;
-
-import static java.util.Objects.requireNonNull;
-import static seedu.address.logic.parser.CliSyntax.*;
-import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
 /**
  * Edits the details of an existing person in the address book.
--- src/main/java/seedu/address/logic/commands/TopCommand.java
+++ src/main/java/seedu/address/logic/commands/TopCommand.java
@@ -14,12 +14,14 @@ public class TopCommand extends Command {
 
     public static final String COMMAND_WORD = "top";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds Top persons whose has the top score in the subject"
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds Top persons whose "
+            + "has the top score in the subject"
             + "Example: " + COMMAND_WORD + " english";
 
     public static final String MESSAGE_TOP_PERSON = "Top %1$s Scorer: %2$s %3$d";
 
-    public static final String MESSAGE_SUBJECT_ERROR = "Please pass in the correct Subject english, science, mathematics, mothertongue or total";
+    public static final String MESSAGE_SUBJECT_ERROR = "Please pass in the correct Subject english, science, 
+            mathematics, mothertongue or total";
 
     public final String subject;
 
@@ -34,55 +36,55 @@ public class TopCommand extends Command {
         int topScore = 0;
         int currentScore = 0;
         List<Person> lastShownList = model.getFilteredPersonList();
-        for(int i=0; i<lastShownList.size();i++){
+        for (int i = 0; i < lastShownList.size(); i++) {
             switch (subject){
 
                 case "english":
-                    if(i==0){
+                    if (i == 0) {
                         topScore = lastShownList.get(i).getEnglish().score;
                     }
                     currentScore = lastShownList.get(i).getEnglish().score;
-                    if(topScore<currentScore) {
+                    if (topScore<currentScore) {
                         topScore = currentScore;
                         index = i;
                     }
                     break;
 
                 case "science":
-                    if(i==0){
+                    if (i == 0) {
                         topScore = lastShownList.get(i).getScience().score;
                     }
                     currentScore = lastShownList.get(i).getScience().score;
-                    if(topScore<currentScore) {
+                    if (topScore<currentScore) {
                         topScore = currentScore;
                         index = i;
                     }
                     break;
 
                 case "mathematics":
-                    if(i==0){
+                    if (i == 0) {
                         topScore = lastShownList.get(i).getMathematics().score;
                     }
                     currentScore = lastShownList.get(i).getMathematics().score;
-                    if(topScore<currentScore) {
+                    if (topScore<currentScore) {
                         topScore = currentScore;
                         index = i;
                     }
                     break;
 
                 case "mothertongue":
-                    if(i==0){
+                    if (i == 0) {
                         topScore = lastShownList.get(i).getMotherTongue().score;
                     }
                     currentScore = lastShownList.get(i).getMotherTongue().score;
-                    if(topScore<currentScore) {
+                    if (topScore<currentScore) {
                         topScore = currentScore;
                         index = i;
                     }
                     break;
 
                 case "total":
-                    if(i==0){
+                    if (i == 0) {
                         topScore = lastShownList.get(i).getEnglish().score+lastShownList.get(i).getMathematics().score+lastShownList.get(i).getMotherTongue().score+lastShownList.get(i).getScience().score;
                     }
                     currentScore = lastShownList.get(i).getEnglish().score+lastShownList.get(i).getMathematics().score+lastShownList.get(i).getMotherTongue().score+lastShownList.get(i).getScience().score;
