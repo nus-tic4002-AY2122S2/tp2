166baeeaaf1d92381b17da3b7db919c6c8a7e601
lowyanshuang <lowyanshuang@gmail.com>
2022-03-29 20:43:29 +0800

    Update default command messages and include dateJoined validation

--- src/main/java/seedu/address/logic/parser/ParserUtil.java
+++ src/main/java/seedu/address/logic/parser/ParserUtil.java
@@ -106,6 +106,9 @@ public class ParserUtil {
     public static DateJoined parseDateJoined(String dateJoined) throws ParseException {
         requireNonNull(dateJoined);
         String trimmedDateJoined = dateJoined.trim();
+        if (!Email.isValidEmail(trimmedDateJoined)) {
+            throw new ParseException(DateJoined.MESSAGE_CONSTRAINTS);
+        }
         return new DateJoined(dateJoined);
     }
 
--- src/main/java/seedu/address/model/person/DateJoined.java
+++ src/main/java/seedu/address/model/person/DateJoined.java
@@ -1,6 +1,7 @@
 package seedu.address.model.person;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
 
 /**
  * Represents a Person's remark in the address book.
@@ -8,6 +9,8 @@ import static java.util.Objects.requireNonNull;
  */
 public class DateJoined {
     public final String value;
+    public static final String MESSAGE_CONSTRAINTS = "Only accept valid date in dd/mm/yyyy format";
+    public static final String VALIDATION_REGEX = "^(((([1-9])|([0-2][0-9])|(3[01]))[\\/]((0[13578])|([13578])|(1[02])))|((([1-9])|([0-2][0-9])|(30))[\\/]((0[469])|([469])|(11)))|((([1-9])|([0-2][0-9]))[\\/](2|02)))[\\/]\\d{4}$|^\\d{4}$";
 
     /**
      * Constructs an {@code dateJoined}.
@@ -16,7 +19,15 @@ public class DateJoined {
      */
     public DateJoined(String dateJoined) {
         requireNonNull(dateJoined);
+        checkArgument(isValidDateJoined(dateJoined), MESSAGE_CONSTRAINTS);
         value = dateJoined;
+    }
+
+    /**
+     * Returns if a given string is a valid email.
+     */
+    public static boolean isValidDateJoined(String test) {
+        return test.matches(VALIDATION_REGEX);
     }
 
     @Override
