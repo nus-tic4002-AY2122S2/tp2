5a42eb2c3ea6c172c707cd43ffdba91981a81d1e
yinyin377 <54065563+yinyin924@users.noreply.github.com>
2022-03-31 23:37:26 +0800

    check for input month within 1 to 12

--- src/main/java/seedu/address/logic/parser/BirthdayCommandParser.java
+++ src/main/java/seedu/address/logic/parser/BirthdayCommandParser.java
@@ -3,7 +3,9 @@ package seedu.address.logic.parser;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 import java.util.Arrays;
+import java.util.logging.Logger;
 
+import seedu.address.commons.core.LogsCenter;
 import seedu.address.logic.commands.BirthdayCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.NameContainsMonthPredicate;
@@ -12,6 +14,8 @@ import seedu.address.model.person.NameContainsMonthPredicate;
  * Parses input arguments and creates a new FindCommand object
  */
 public class BirthdayCommandParser implements Parser<BirthdayCommand> {
+
+    private static final Logger logger = LogsCenter.getLogger(BirthdayCommandParser.class);
 
     /**
      * Parses the given {@code String} of arguments in the context of the FindCommand
@@ -26,6 +30,18 @@ public class BirthdayCommandParser implements Parser<BirthdayCommand> {
         }
 
         String[] nameKeywords = trimmedArgs.split("\\s+");
+        for (String temp: nameKeywords) {
+            try{
+                int month = Integer.parseInt(temp);
+                if (month < 1 || month > 12) {
+                    throw new ParseException(BirthdayCommand.MESSAGE_INVALID_MONTH);
+                }
+            }
+            catch (NumberFormatException ex){
+                ex.printStackTrace();
+            }
+        }
+        logger.info("======================[ Bday Parse with arguments ] Month " + nameKeywords + " ===========");
 
         return new BirthdayCommand(new NameContainsMonthPredicate(Arrays.asList(nameKeywords)));
     }
