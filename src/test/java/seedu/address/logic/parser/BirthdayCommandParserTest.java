955718923fee8ae636d590c9b3654ac6d862a9d5
yinyin377 <54065563+yinyin924@users.noreply.github.com>
2022-04-10 20:00:32 +0800

    add BirthdayCommandParserTest, FilterCommandParserTest, RenameCommandParserTest and ParserUtilTest

---
+++ src/test/java/seedu/address/logic/parser/BirthdayCommandParserTest.java
@@ -0,0 +1,33 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.ParserUtilTest.assertParseFailure;
+import static seedu.address.logic.parser.ParserUtilTest.assertParseSuccess;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.commands.BirthdayCommand;
+import seedu.address.model.person.NameContainsMonthPredicate;
+
+
+class BirthdayCommandParserTest {
+    private BirthdayCommandParser parser = new BirthdayCommandParser();
+
+    @Test
+    void parse() {
+        // null -> returns parseException
+        assertParseFailure(parser, "     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT, BirthdayCommand.MESSAGE_USAGE));
+
+        // less than 1 or more than 12 -> returns parseException
+        assertParseFailure(parser, "0", BirthdayCommand.MESSAGE_INVALID_MONTH);
+        assertParseFailure(parser, "13", BirthdayCommand.MESSAGE_INVALID_MONTH);
+
+        //between 1 and 12 -> returns success
+        BirthdayCommand expectedCommand =
+                new BirthdayCommand(new NameContainsMonthPredicate(Arrays.asList("1", "12")));
+        assertParseSuccess(parser, "1 12", expectedCommand);
+
+    }
+}
