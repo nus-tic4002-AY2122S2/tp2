0d253ab4bbddd077bf505c556478e9d9c3fc1e38
Tan Chia Chun <54241577+TanChiaChun@users.noreply.github.com>
2022-04-11 15:54:14 +0800

    Move Money Overview update to LogicManager execute()

--- src/main/java/seedu/address/logic/commands/DeleteCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteCommand.java
@@ -42,9 +42,6 @@ public class DeleteCommand extends Command {
         Person personToDelete = lastShownList.get(targetIndex.getZeroBased());
         model.deletePerson(personToDelete);
 
-        model.updateTotalMoney();
-        model.updateContactsWithMoneyCount();
-
         return new CommandResult(String.format(MESSAGE_DELETE_PERSON_SUCCESS, personToDelete));
     }
 
